5f91a97f834c41bda41f8a718805f225a7e4fc36
sm000k <smok.lacki@gmail.com>
2023-03-14 23:05:19 +0100

    first example with pool success

--- second_serverless_start.py
+++ second_serverless_start.py
@@ -1,19 +1,42 @@
 import time
+from lithops import ServerlessExecutor
 
-from lithops import FunctionExecutor
+from lithops.multiprocessing import Pool
+
+
+# setup_lithops_logger(logging.CRITICAL)
 
 
 def sample_method(data):
-    data
-    return time.process_time()
+    time_elapsed = time.localtime()
+    return time_elapsed
 
 
-def hello(name):
-    return 'Hello {}!'.format(name)
+def execute_sample_method_remotely():
+    fexec = ServerlessExecutor()
+    fut = fexec.call_async(sample_method, 'DATA')
+    print('Waiting for the result...')
+    result = fut.result()
+    print('Result: ', result)
+
+def hello(input):
+    return 'Hello,{}!'.format(input)
+def square(x):
+    return x * x
 
 
-with FunctionExecutor() as fexec:
-    # fut = fexec.call_async(hello, 'World')
-    # print(fut.result())
-    fut2 = fexec.call_async(sample_method,'DATA')
-    print(fut2.result())
+def divide(x, y):
+    return x / y
+
+
+def sleep_seconds(s):
+    time.sleep(s)
+
+execute_sample_method_remotely()
+
+if __name__ == '__main__':
+
+  with Pool() as pool:
+      # Synchronously execute function remotely
+      res = pool.apply(hello,('World',))
+      print(res) #print "Hello World"
